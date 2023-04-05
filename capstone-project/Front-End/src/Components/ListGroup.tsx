5f1f644fbc89823e323dae4018edaef9c71fa67c
Jeziel18 <jeziel.torres1@upr.edu>
2023-04-05 09:23:51 -0400

    Enviroment Setup for Front End

---
+++ capstone-project/Front-End/src/Components/ListGroup.tsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
